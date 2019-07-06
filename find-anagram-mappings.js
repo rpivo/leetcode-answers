1a38b23548fee2389bebc62b3b253c47914b7665
Ryan Pivovar <ryanpivovar@gmail.com>
2019-07-06 16:09:09 -0700

    add comments to find-anagram-mappings

--- find-anagram-mappings.js
+++ find-anagram-mappings.js
@@ -1,4 +1,13 @@
 /*
+ * an anagram is a word, phrase, or name formed by rearranging the letters of another, such as
+ * 'cinema', from 'iceman'.
+ * 
+ * in this problem, we are given two arrays: A and B. B is an anagram of A, meaning that B is made
+ * by randomizing the order of the elements in A.
+ * 
+ * we want to loop through the items in array A and find the index location of that value in array
+ * B.
+ *
  * @param {number[]} A
  * @param {number[]} B
  * @return {number[]} 
