c83270dc87fcb8f39e5ef0649522064e9d8ffbc2
Ryan Pivovar <ryanpivovar@gmail.com>
2019-07-08 13:27:07 -0700

    add initial code for fixed-point

---
+++ fixed-point.js
@@ -0,0 +1,12 @@
+/*
+ * given an array A of distinct integers sorted in ascending order, return the smallest index i
+ * that satisfies A[i] == i . 
+ */
+
+var fixedPoint = function(A) {
+  return 'floop';
+};
+
+A = [ 1, 2, 3, 4, 5 ];
+
+console.log(fixedPoint(A));
