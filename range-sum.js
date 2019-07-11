17687e0862beaa90a2de4579a7092462d41ed0f5
Ryan Pivovar <ryanpivovar@gmail.com>
2019-07-10 17:11:13 -0700

    set up range-sum

---
+++ range-sum.js
@@ -0,0 +1,18 @@
+/*
+ * given the root node of a binary search tree, return the sum of values of all nodes with value
+ * between L and R (inclusive). The binary search tree is guaranteed to have unique values. 
+ * 
+ * a binary search tree is a node-based binary tree data structure which has the following properties:
+ * - the left subtree of a node contains only nodes with keys lesser than the node's key.
+ * - the right subtree of a node contains only nodes with keys greater than the node's key.
+ * - the left and right subtree each must also be a binary search tree.
+ * 
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+
+ var rangeSumBST = function(root, L, R) {
+
+ };
