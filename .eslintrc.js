22cf5a6f9414e54584d5a9725f97871a4c8b805d
Ryan Pivovar <ryanpivovar@gmail.com>
2019-07-13 22:51:03 -0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
-    "extends": "airbnb"
-};
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "comma-dangle": [2, {
+      "arrays": "always",
+      "functions": "never",
+      "objects": "always",
+    }],
+    "jsx-quotes": ["error", "prefer-single"],
+    "max-len": 0,
+    "no-confusing-arrow": 0,
+    "no-param-reassign": ["error", { "props": false }],
+    "no-nested-ternary": 0,
+    "no-undef": 0,
+    "react/destructuring-assignment": 0,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/no-unescaped-entities": 0,
+    "react/prop-types": 0,
+  }
+}
--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 /*
  * the below code is an implementation of the binary search tree data structure in javascript.
- * 
- * a tree is a non-linear data structure that is a collection of nodes. 
- * 
+ *
+ * a tree is a non-linear data structure that is a collection of nodes.
+ *
  * a binary search tree is a binary tree in which nodes that have a lesser value are stored on the
  * left, while nodes with a greater value are stored on the right.
- * 
+ *
  * to make nodes for the binary search tree, we can define a Node class. the Node class has three
  * properties: data, left, and right. left and right are pointers to the left and right nodes in a
  * binary search tree.
- * 
+ *
  * when an object of Node is created, (data) is passed in as a value. it's stored within the
  * instance's data property under certain conditions. Both the left and right properties are
  * initially set to null.
  */
 // Node class
 class Node {
-  constructor (data) {
+  constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
@@ -25,11 +25,11 @@ class Node {
 
 /*
  * example of a binary search tree class. this is just the framework. we will have to build these
- * methods: 
- * 
+ * methods:
+ *
  * insert(data)
  * remove(data)
- * 
+ *
  * helper functions:
  * findMinNode()
  * getRootNode()
@@ -37,8 +37,8 @@ class Node {
  * preorder(node)
  * postorder(node)
  * search(node, data)
- * 
- * the BinarySearchTree class contains a private variable `root`, which holds the root of a tree. 
+ *
+ * the BinarySearchTree class contains a private variable `root`, which holds the root of a tree.
  * `root` is initialzed to null.
  */
 class BinarySearchTree {
@@ -47,32 +47,26 @@ class BinarySearchTree {
   }
 
   insert(data) {
-
-    var newNode = new Node(data);
+    const newNode = new Node(data);
 
     if (this.root === null) {
       this.root = newNode;
     } else {
       this.insertNode(this.root, newNode);
     }
-
   }
 
   insertNode(node, newNode) {
-
     if (newNode.data < node.data) {
       if (node.left === null) {
         node.left = newNode;
       } else {
         this.insertNode(node.left, newNode);
       }
+    } else if (node.right === null) {
+      node.right = newNode;
     } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
+      this.insertNode(node.right, newNode);
     }
-
   }
 }
