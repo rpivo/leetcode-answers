dca723eb2e56dbcc5a07fd8df7c66724aba9b48e
Ryan Pivovar <ryanpivovar@gmail.com>
2019-09-29 06:42:09 -0700

    update unique-number-of-occurrences

--- unique-number-of-occurrences.js
+++ unique-number-of-occurrences.js
@@ -1,13 +1,11 @@
 var uniqueOccurrences = function(arr) {
   const set = new Set(arr);
   let lengths = [];
-  set.forEach((num) => {
-    lengths.push(arr.filter(x => x === num).length);
-  });
+  set.forEach(num => lengths.push(arr.filter(x => x === num).length));
   lengths = new Set(lengths);
   return (lengths.size === set.size) ? true : false;
 };
 
 const arr = [-3,0,1,-3,1,1,1,-3,10,0];
 
-uniqueOccurrences(arr);
+console.log(uniqueOccurrences(arr));
