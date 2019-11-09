653bfb061bf6c5ffffad8ed3688f25b06d696d39
Ryan Pivovar <ryanpivovar@gmail.com>
2019-11-08 19:36:34 -0800

    add sql

---
+++ immediate-food-delivery-1-INSERT.sql
@@ -0,0 +1,24 @@
+INSERT INTO delivery
+    (delivery_id, customer_id, order_date, customer_pref_delivery_date)
+VALUES
+    ('1', '1', '2019-08-01', '2019-08-02');
+INSERT INTO delivery
+    (delivery_id, customer_id, order_date, customer_pref_delivery_date)
+VALUES
+    ('2', '5', '2019-08-02', '2019-08-02');
+INSERT INTO delivery
+    (delivery_id, customer_id, order_date, customer_pref_delivery_date)
+VALUES
+    ('3', '1', '2019-08-11', '2019-08-11');
+INSERT INTO delivery
+    (delivery_id, customer_id, order_date, customer_pref_delivery_date)
+VALUES
+    ('4', '3', '2019-08-24', '2019-08-26');
+INSERT INTO delivery
+    (delivery_id, customer_id, order_date, customer_pref_delivery_date)
+VALUES
+    ('5', '4', '2019-08-21', '2019-08-22');
+INSERT INTO delivery
+    (delivery_id, customer_id, order_date, customer_pref_delivery_date)
+VALUES
+    ('6', '2', '2019-08-11', '2019-08-13');
