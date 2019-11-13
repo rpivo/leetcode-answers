42919f4d9d11bcf1f1482fa0944b27ee0b56b25c
Ryan Pivovar <ryanpivovar@gmail.com>
2019-11-13 14:40:43 -0800

    add insert query for reformat department table

---
+++ reformat-department-table-INSERT.sql
@@ -0,0 +1,20 @@
+INSERT INTO department
+    (id, revenue, month)
+VALUES
+    ('1', '8000', 'Jan');
+INSERT INTO department
+    (id, revenue, month)
+VALUES
+    ('2', '9000', 'Jan');
+INSERT INTO department
+    (id, revenue, month)
+VALUES
+    ('3', '10000', 'Feb');
+INSERT INTO department
+    (id, revenue, month)
+VALUES
+    ('1', '7000', 'Feb');
+INSERT INTO department
+    (id, revenue, month)
+VALUES
+    ('1', '6000', 'Mar');
