29af06bea25f45a46beeafa67d8a7be9871be827
Ryan Pivovar <ryanpivovar@gmail.com>
2020-07-26 11:26:08 -0400

    solve remove-vowels-from-a-string

--- remove-vowels-from-a-string.ts
+++
@@ -1,7 +0,0 @@
-function removeVowels(S: string): string {
-  let match;
-  while ((match = /[aeiou]/.exec(S)) !== null) {
-    S = S.slice(0, match.index) + S.slice(match.index + 1, S.length);
-  }
-  return S;
-};
