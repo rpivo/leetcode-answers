2d8552b4d0a7036bdf38bbae140801bae73cc3c6
Ryan <ryanpivovar@gmail.com>
2021-04-20 20:36:14 -0400

    solve find-customers-with-positive-revenue-this-year.sql

---
+++ find-customers-with-positive-revenue-this-year-2.sql
@@ -0,0 +1,5 @@
+-- From the Customers table
+-- select the customer_id column
+-- where the revenue is greater than 0
+-- and where year is equal to 2021
+SELECT customer_id FROM Customers WHERE revenue > 0 AND year = 2021;
