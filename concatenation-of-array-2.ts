d2ee95266dbbe2661869c9966a61860113fdd621
Ryan <ryanpivovar@gmail.com>
2021-09-24 20:13:05 -0400

    solve concatenation-of-array.ts

---
+++ concatenation-of-array-2.ts
@@ -0,0 +1,6 @@
+// define function getConcatenation, which takes in an array of numbers called
+// nums, and returns an array of numbers
+function getConcatenation(nums: number[]): number[] {
+  // return nums concatenated with itself
+  return nums.concat(nums);
+}
