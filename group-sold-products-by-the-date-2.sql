979b05822a88ed402e25da4f4d6b16249016764e
Ryan <ryanpivovar@gmail.com>
2021-12-09 17:26:19 -0500

    solve group-sold-products-by-the-date.sql (partial)

---
+++ group-sold-products-by-the-date-2.sql
@@ -0,0 +1,8 @@
+SELECT sell_date,
+  COUNT(DISTINCT product) as num_sold,
+  GROUP_CONCAT(
+    DISTINCT product
+    ORDER BY product SEPARATOR ','
+  ) as products
+FROM Activities
+GROUP BY sell_date;
